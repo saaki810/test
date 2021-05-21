a577ef2e951a419765c79b93497bbb7faf59088b
saiki <koperist@gmail.com>
2021-05-21 22:53:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/map_memo/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.map_memo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
+    private lateinit var mMap:GoogleMap
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION=1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //画面スリープdiable
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        val mapFragment=supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap:GoogleMap){
+        mMap=googleMap
+
+        checkPermission()
+    }
+
+    private fun checkPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+            myLocationEnable()
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            //許可もとめ、拒否されてたとき
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        }else{
+            //まだ許可求めていない
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION->{
+                if(permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //許可
+                    myLocationEnable()
+                }else{
+                    showToast("現在位置は表示せきません。")
+                }
+
+            }
+        }
+    }
+
+    private fun myLocationEnable(){
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mMap.isMyLocationEnabled=true
+        }
+    }
+
+    private fun showToast(msg:String){
+        val toast=Toast.makeText(this,msg,Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+}
